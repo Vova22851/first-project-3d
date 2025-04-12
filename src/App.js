f56793a055d5179526fd90206e17ca70be4ec808
John Doe <johndoe@example.com>
2025-04-12 16:40:55 +0300

    3d model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
-import React from "ract";
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from '@react-three/drei';
+
+
+function RotatingBox() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current){
+    meshRef.current.rotation.x += 0.01
+    meshRef.current.rotation.y += 0.01
+    }
+  });
+
+  return(
+    <mesh ref={meshRef}>
+    <boxGeometry args={[2,2,2]}/>
+    <meshStandardMaterial color="red"/>
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Template</h1>
-      <p>Start template for React-projects</p>
+    <div id="canvas-countainer" className="App">
+      <Canvas>
+        <RotatingBox />
+          <ambientLight intensity={0.2}/>
+          <directionalLight color="yellow" position={[0,2,5]} />
+          <OrbitControls />
+      </Canvas>
     </div>
   );
 }
 
+createRoot(document.getElementById('root')).render(<App />)
 export default App;
